14a132deb7b8bb7a1d0f5822ff168ba8df577866
AirCaptainx99 <jeffryco_ardiya@yahoo.com>
2024-04-21 23:18:27 +0700

    test

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-() => {
-    console.log("Hello World");
+let sessionName = sessionStorage.getItem("name");
+
+if (sessionName === null){
+    document.querySelector("div#content").innerHTML = "Sorry, you don't seem to have created a session."
+}
+else{
+    document.querySelector("div#content").innerHTML = "<a href=\"./index.html\"><p>Back to index.html</p></a>"
 }
